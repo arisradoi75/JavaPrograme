8761dada0b92eca79d18e34ebdaba54038bdef20
arisradoi <radoiaris14@gmail.com>
2025-10-02 16:53:43 +0300

    Ex4

--- JavaFacultate/src/Ex4/MainApp.java
+++ JavaFacultate/src/Ex4/MainApp.java
@@ -3,9 +3,27 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class MainApp {
+    /*Să se determine cmmdc a două numere naturale, a căror valoare maximă este 30. Numerele
+        vor fi generate aleatoriu cu ajutorul unui obiect de tip Random și metodei nextInt();*/
+        public static int cmmdc(int a , int b){
+            while(b != 0){ // punem conditia ca unu din nr sa fie diferit de 0;
+                int r =  a % b;
+                a = b;
+                b = r;
+            }
+            return a;
+        }
+
     public static void main(String[] args) {
+        Random r = new Random();
 
+        int a = r.nextInt(31);
+        int b = r.nextInt(31);
 
+        int rezultat = cmmdc(a , b);
+        System.out.println("a=" + a + " b=" + b);
+        System.out.println(rezultat);
 
     }
+
 }
