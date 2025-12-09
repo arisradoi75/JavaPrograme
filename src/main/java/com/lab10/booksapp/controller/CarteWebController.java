8dbc86603b793040a650e1b8478e54f59e3892c5
arisradoi <radoiaris14@gmail.com>
2025-12-09 09:06:34 +0200

    add a book function

--- src/main/java/com/lab10/booksapp/controller/CarteWebController.java
+++ src/main/java/com/lab10/booksapp/controller/CarteWebController.java
@@ -1,19 +1,25 @@
 package com.lab10.booksapp.controller;
 
+import com.lab10.booksapp.model.Carte;
 import com.lab10.booksapp.repository.CarteRepository;
+import com.lab10.booksapp.service.CarteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CarteWebController {
 
-    private final CarteRepository carteRepository;
+    private CarteRepository carteRepository;
+    private CarteService carteService;
+
 
     @Autowired
-    public CarteWebController(CarteRepository carteRepository) {
+    public CarteWebController(CarteRepository carteRepository, CarteService carteService) {
         this.carteRepository = carteRepository;
+        this.carteService = carteService;
     }
 
     @GetMapping("/lista-carti")
@@ -21,6 +27,16 @@ public class CarteWebController {
         String s = "Lista cartilor";
 
         model.addAttribute("str" , s);
+        model.addAttribute("carti", carteRepository.findAll());
+
+        return "lista-carti";
+    }
+
+    @GetMapping("/adauga-carte")
+    public String adaugaCarte(@RequestParam String isbn, @RequestParam String titlu, @RequestParam String autor ,Model model) {
+        Carte carte = new Carte(isbn , titlu , autor);
+        carteService.adaugaCarte(carte);
+        model.addAttribute("mesaj", "Carte adaugata cu succes!");
         model.addAttribute("carti", carteRepository.findAll());
 
         return "lista-carti";
---
+++ src/main/java/com/lab10/booksapp/service/CarteService.java
@@ -0,0 +1,22 @@
+package com.lab10.booksapp.service;
+
+import com.lab10.booksapp.model.Carte;
+import com.lab10.booksapp.repository.CarteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarteService {
+
+    @Autowired
+    CarteRepository carteRepository;
+    public CarteService(CarteRepository carteRepository) {
+        this.carteRepository = carteRepository;
+    }
+
+    public void adaugaCarte(Carte carte) {
+        carteRepository.save(carte);
+    }
+
+
+}
